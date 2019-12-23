1990ff69465090b859fb2326915107ab8183cd5e
Mikolaj Markiewicz <kajo100@gmail.com>
2019-12-23 22:58:23 +0100

    Init webapp project and model interfaces

---
+++ api-client/model/src/main/java/pl/edu/pw/ddm/api/model/Model.java
@@ -0,0 +1,5 @@
+package pl.edu.pw.ddm.api.model;
+
+import java.io.Serializable;
+
+public interface Model extends Serializable {}
---
+++ api-client/src/test/java/pl/edu/pw/ddm/api/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.ddm.api.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

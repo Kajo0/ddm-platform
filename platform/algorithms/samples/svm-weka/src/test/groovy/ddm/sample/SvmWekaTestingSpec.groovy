3d8bd9c8b9988aacfcbaa934708602496d4350c8
Mikolaj Markiewicz <kajo100@gmail.com>
2020-05-30 01:25:49 +0200

    Distribution testing runner used in SVM-WEKA project.

---
+++ platform/algorithms/samples/svm-weka/src/test/groovy/ddm/sample/SvmWekaTestingSpec.groovy
@@ -0,0 +1,52 @@
+package ddm.sample
+
+import pl.edu.pw.ddm.platform.testing.interfaces.impl.CentralRunner
+import pl.edu.pw.ddm.platform.testing.interfaces.impl.ExecutionConfig
+import spock.lang.Specification
+
+class SvmWekaTestingSpec extends Specification {
+
+    def "should perform correct classification of training data"() {
+        given:
+        def algorithm = new SvmWeka()
+        def classifier = new WekaClassifier()
+
+        and:
+        def config = ExecutionConfig.builder()
+                .localProcessor(algorithm)
+                .globalProcessor(algorithm)
+                .miningMethod(classifier)
+                .dataPath([getClass().getResource('/data.train').path])
+                .testDataPath(getClass().getResource('/data.train').path)
+                .separator(',')
+                .idIndex(0)
+                .labelIndex(1)
+                .attributesAmount(4)
+                .colTypes(['numeric', 'nominal', 'numeric', 'numeric', 'numeric', 'numeric'] as String[])
+                .distanceFunction(null)
+                .executionParams(['options': '-C 12.5 -L 0.001 -P 1.0E-12 -N 0 -V -1 -W 1 -K "weka.classifiers.functions.supportVector.RBFKernel -C 250007 -G 0.50625"'])
+                .build()
+        def cr = new CentralRunner(config)
+
+        when:
+        def results = cr.run()
+
+        then:
+        results.results.size() == 8
+
+        and:
+        results.results[0].value == ' A'
+        results.results[1].value == ' A'
+        results.results[2].value == ' A'
+        results.results[3].value == ' A'
+        results.results[4].value == ' B'
+        results.results[5].value == ' B'
+        results.results[6].value == ' B'
+        results.results[7].value == ' B'
+
+        and: "print results"
+        results.results
+                .each { println it }
+    }
+
+}

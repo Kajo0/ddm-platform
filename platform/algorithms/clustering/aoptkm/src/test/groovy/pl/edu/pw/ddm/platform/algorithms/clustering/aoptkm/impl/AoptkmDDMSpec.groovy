008e476afaec75efca8c8256400666d23a00fcc1
Mikolaj Markiewicz <kajo100@gmail.com>
2020-06-03 00:15:38 +0200

    Use of testing-interfaces-impl. for AOPTKM algorithm impl.

---
+++ platform/algorithms/clustering/aoptkm/src/test/groovy/pl/edu/pw/ddm/platform/algorithms/clustering/aoptkm/impl/AoptkmDDMSpec.groovy
@@ -0,0 +1,86 @@
+package pl.edu.pw.ddm.platform.algorithms.clustering.aoptkm.impl
+
+import pl.edu.pw.ddm.platform.distfunc.EuclideanDistance
+import pl.edu.pw.ddm.platform.interfaces.data.DataProvider
+import pl.edu.pw.ddm.platform.metrics.ClusteringMetrics
+import pl.edu.pw.ddm.platform.metrics.dto.IdLabel
+import pl.edu.pw.ddm.platform.testing.interfaces.impl.CentralRunner
+import pl.edu.pw.ddm.platform.testing.interfaces.impl.ExecutionConfig
+import pl.edu.pw.ddm.platform.testing.interfaces.impl.data.NodeDataProvider
+import spock.lang.Specification
+
+class AoptkmDDMSpec extends Specification {
+
+    def "should perform correct clustering of training data"() {
+        given:
+        def localProcessor = new AoptkmDDM()
+        def globalProcessor = localProcessor
+        def miningMethod = localProcessor
+
+        and:
+        def config = ExecutionConfig.builder()
+                .localProcessor(localProcessor)
+                .globalProcessor(globalProcessor)
+                .miningMethod(miningMethod)
+                .dataPath([
+                        getClass().getResource('/iris.train').path,
+                        getClass().getResource('/iris.test').path
+                ])
+                .testDataPath(getClass().getResource('/data_dummy.test').path)
+                .separator(',')
+                .idIndex(0)
+                .labelIndex(5)
+                .attributesAmount(4)
+                .colTypes(null) // null to use deduct
+                .distanceFunction(new EuclideanDistance())
+                .executionParams([
+                        'groups'    : '3',
+                        'iterations': '10'
+                ])
+                .build()
+        def cr = new CentralRunner(config)
+
+        when:
+        def results = cr.run()
+
+        then:
+        results.results.size() == 150
+
+        and:
+        def desc = DataProvider.DataDesc.builder()
+                .separator(config.separator)
+                .idIndex(config.idIndex)
+                .labelIndex(config.labelIndex)
+                .attributesAmount(config.attributesAmount)
+                .colTypes(config.colTypes)
+                .build()
+        def labels = config.dataPath
+                .collectMany { path ->
+                    new NodeDataProvider(
+                            path,
+                            config.testDataPath,
+                            desc,
+                            true
+                    ).training()
+                }.collect { IdLabel.of(it.id, it.label) }
+
+        and:
+        def predictions = results.results
+                .collect { IdLabel.of(it.id, it.value) }
+
+        def metrics = new ClusteringMetrics(predictions, labels)
+        def ari = metrics.adjustedRandIndex()
+        ari >= 0
+        ari <= 1
+
+        and: "print results"
+        println("ARI: $ari")
+
+        and:
+        def groups = results.results
+                .collect { it.value }
+                .unique()
+        println("Groups: $groups")
+    }
+
+}

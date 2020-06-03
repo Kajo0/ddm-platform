dae33f6151f50658e47a0c1a3e37f837b304294b
Mikolaj Markiewicz <kajo100@gmail.com>
2020-06-03 21:42:49 +0200

    More test data set R15 for LSC and AOPTKM.

--- platform/algorithms/clustering/aoptkm/src/test/groovy/pl/edu/pw/ddm/platform/algorithms/clustering/aoptkm/impl/AoptkmDDMSpec.groovy
+++ platform/algorithms/clustering/aoptkm/src/test/groovy/pl/edu/pw/ddm/platform/algorithms/clustering/aoptkm/impl/AoptkmDDMSpec.groovy
@@ -11,7 +11,7 @@ import spock.lang.Specification
 
 class AoptkmDDMSpec extends Specification {
 
-    def "should perform correct clustering of training data"() {
+    def "should perform correct clustering of iris training data"() {
         given:
         def localProcessor = new AoptkmDDM()
         def globalProcessor = localProcessor
@@ -80,7 +80,80 @@ class AoptkmDDMSpec extends Specification {
         def groups = results.results
                 .collect { it.value }
                 .unique()
-        println("Groups: $groups")
+        println("${groups.size()} groups: $groups")
+    }
+
+    def "should perform correct clustering of R15 training data"() {
+        given:
+        def localProcessor = new AoptkmDDM()
+        def globalProcessor = localProcessor
+        def miningMethod = localProcessor
+
+        and:
+        def config = ExecutionConfig.builder()
+                .localProcessor(localProcessor)
+                .globalProcessor(globalProcessor)
+                .miningMethod(miningMethod)
+                .dataPath([
+                        getClass().getResource('/R15_node_1.data').path,
+                        getClass().getResource('/R15_node_2.data').path,
+                        getClass().getResource('/R15_node_3.data').path
+                ])
+                .testDataPath(getClass().getResource('/data_dummy.test').path)
+                .separator('\t')
+                .idIndex(0)
+                .labelIndex(3)
+                .attributesAmount(2)
+                .colTypes(null) // null to use deduct
+                .distanceFunction(new EuclideanDistance())
+                .executionParams([
+                        'groups'    : '15',
+                        'iterations': '10',
+                ])
+                .build()
+        def cr = new CentralRunner(config)
+
+        when:
+        def results = cr.run()
+
+        then:
+        results.results.size() == 600
+
+        and:
+        def desc = DataProvider.DataDesc.builder()
+                .separator(config.separator)
+                .idIndex(config.idIndex)
+                .labelIndex(config.labelIndex)
+                .attributesAmount(config.attributesAmount)
+                .colTypes(config.colTypes)
+                .build()
+        def labels = config.dataPath
+                .collectMany { path ->
+                    new NodeDataProvider(
+                            path,
+                            config.testDataPath,
+                            desc,
+                            true
+                    ).training()
+                }.collect { IdLabel.of(it.id, it.label) }
+
+        and:
+        def predictions = results.results
+                .collect { IdLabel.of(it.id, it.value) }
+
+        def metrics = new ClusteringMetrics(predictions, labels)
+        def ari = metrics.adjustedRandIndex()
+        ari >= 0
+        ari <= 1
+
+        and: "print results"
+        println("ARI: $ari")
+
+        and:
+        def groups = results.results
+                .collect { it.value }
+                .unique()
+        println("${groups.size()} groups: $groups")
     }
 
 }

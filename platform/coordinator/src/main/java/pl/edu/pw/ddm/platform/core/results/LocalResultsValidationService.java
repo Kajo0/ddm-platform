47020527a05fe05fe77ba3db2572a311b6555d43
Mikolaj Markiewicz <kajo100@gmail.com>
2020-12-05 12:52:48 +0100

    Saving validation metrics in results dir.

--- platform/coordinator/src/main/java/pl/edu/pw/ddm/platform/core/results/LocalResultsValidationService.java
+++ platform/coordinator/src/main/java/pl/edu/pw/ddm/platform/core/results/LocalResultsValidationService.java
@@ -1,16 +1,23 @@
 package pl.edu.pw.ddm.platform.core.results;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import pl.edu.pw.ddm.platform.core.data.DataFacade;
 import pl.edu.pw.ddm.platform.core.data.dto.DataDescDto;
@@ -22,9 +29,16 @@ import pl.edu.pw.ddm.platform.metrics.Metrics;
 import pl.edu.pw.ddm.platform.metrics.dto.IdLabel;
 import pl.edu.pw.ddm.platform.metrics.dto.MetricsSummary;
 
+@Slf4j
 @Service
-@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 class LocalResultsValidationService implements ResultsValidationService {
+
+    @Value("${paths.results.path}")
+    private String resultsPath;
+
+    @Value("${paths.results.validation-filename}")
+    private String validationFilename;
 
     private final DataFacade dataFacade;
     private final ExecutionResultsFacade executionResultsFacade;
@@ -47,15 +61,22 @@ class LocalResultsValidationService implements ResultsValidationService {
                 .flatMap(Collection::stream)
                 .collect(Collectors.toUnmodifiableList());
 
+        MetricsSummary summary;
         switch (algorithmType) {
             case MiningMethod.MethodType.CLUSTERING:
-                return clusteringValidation(predictions, target, metrics);
+                summary = clusteringValidation(predictions, target, metrics);
+                break;
             case MiningMethod.MethodType.CLASSIFICATION:
-                return classificationValidation(predictions, target, metrics);
+                summary = classificationValidation(predictions, target, metrics);
+                break;
 
             default:
                 throw new IllegalArgumentException("Unknown algorithm type: " + algorithmType);
         }
+
+        saveMetrics(summary, executionId);
+
+        return summary;
     }
 
     private MetricsSummary clusteringValidation(List<IdLabel> predictions, List<IdLabel> target, String[] metrics) {
@@ -97,6 +118,30 @@ class LocalResultsValidationService implements ResultsValidationService {
                 .map(line -> line.split(separator))
                 .map(idLabel -> IdLabel.of(idLabel[idIndex].trim(), idLabel[labelIndex].trim()))
                 .collect(Collectors.toUnmodifiableList());
+    }
+
+    private void saveMetrics(MetricsSummary summary, String executionId) throws IOException {
+        ValidationMetrics metrics = loadMetrics(executionId);
+        metrics.getMetrics()
+                .putAll(summary.getMetrics());
+
+        Path path = Paths.get(resultsPath, executionId, validationFilename);
+        log.info("Saving file '{}' with execution validation metrics.", path);
+        String json = new ObjectMapper().writerWithDefaultPrettyPrinter()
+                .writeValueAsString(metrics);
+        Files.write(path, json.getBytes());
+    }
+
+    @SneakyThrows
+    private ValidationMetrics loadMetrics(String executionId) {
+        Path path = Paths.get(resultsPath, executionId, validationFilename);
+        if (path.toFile().exists()) {
+            log.info("Loading validation metrics file for execution id: '{}'.", executionId);
+            return new ObjectMapper().readValue(path.toFile(), ValidationMetrics.class);
+        } else {
+            log.info("Validation metrics file for execution id: '{}' not exists.", executionId);
+            return new ValidationMetrics();
+        }
     }
 
 }
--- platform/coordinator/src/main/java/pl/edu/pw/ddm/platform/core/results/ResultsValidationService.java
+++ platform/coordinator/src/main/java/pl/edu/pw/ddm/platform/core/results/ResultsValidationService.java
@@ -1,10 +1,20 @@
 package pl.edu.pw.ddm.platform.core.results;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.Data;
 import lombok.NonNull;
 import pl.edu.pw.ddm.platform.metrics.dto.MetricsSummary;
 
 interface ResultsValidationService {
 
     MetricsSummary validate(@NonNull String executionId, String dataId, String algorithmType, String[] metrics);
+
+    @Data
+    class ValidationMetrics {
+
+        private Map<String, Double> metrics = new HashMap<>();
+    }
 
 }

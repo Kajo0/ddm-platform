4f981f9b3ed57a97811cfa0bb125a21fd7c765e6
Mikolaj Markiewicz <kajo100@gmail.com>
2021-03-16 19:34:42 +0100

    LCT initiazliation param for k-means

--- platform/algorithms/clustering/lct/src/main/java/pl/edu/pw/ddm/platform/algorithms/clustering/lct/KmeansWrapper.java
+++ platform/algorithms/clustering/lct/src/main/java/pl/edu/pw/ddm/platform/algorithms/clustering/lct/KmeansWrapper.java
@@ -12,6 +12,8 @@ import weka.clusterers.SimpleKMeans;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.Instances;
+import weka.core.SelectedTag;
+import weka.core.Tag;
 
 class KmeansWrapper {
 
@@ -35,7 +37,10 @@ class KmeansWrapper {
         int groups = paramProvider.provideNumeric("groups").intValue();
         int iterations = paramProvider.provideNumeric("iterations").intValue();
         int seed = paramProvider.provideNumeric("seed", (double) new Random().nextInt(Integer.MAX_VALUE)).intValue();
-        printConfig(groups, iterations, seed);
+        String initMethod = paramProvider.provide("init_kmeans_method", "Random");
+        SelectedTag method = findSelectedTag(initMethod);
+
+        printConfig(groups, iterations, seed, initMethod);
 
         SimpleKMeans kmeans = new SimpleKMeans();
         kmeans.setNumClusters(groups);
@@ -44,18 +49,34 @@ class KmeansWrapper {
         kmeans.setSeed(seed);
         kmeans.setDisplayStdDevs(true);
 
+        if (initMethod != null) {
+            kmeans.setInitializationMethod(method);
+        }
+
         kmeans.buildClusterer(dataset);
 
         return kmeans;
     }
 
-    private static void printConfig(int groups, int iterations, int seed) {
+    private SelectedTag findSelectedTag(String initMethod) {
+        for (Tag tag : SimpleKMeans.TAGS_SELECTION) {
+            if (tag.getReadable().equals(initMethod)) {
+                System.out.println("  [[FUTURE LOG]] Found tag method for '" + initMethod + "'");
+                return new SelectedTag(tag.getID(), SimpleKMeans.TAGS_SELECTION);
+            }
+        }
+        System.out.println("  [[FUTURE LOG]] Not found tag method for '" + initMethod + "' so using default.");
+        return null;
+    }
+
+    private static void printConfig(int groups, int iterations, int seed, String initMethod) {
         System.out.println("---------------------------------");
         System.out.println("-     K-means WEKA - CONFIG     -");
         System.out.println("---------------------------------");
         System.out.println("  groups        = " + groups);
         System.out.println("  iterations    = " + iterations);
         System.out.println("  seed          = " + seed);
+        System.out.println("  initMethod    = " + initMethod);
         System.out.println("---------------------------------");
     }
 

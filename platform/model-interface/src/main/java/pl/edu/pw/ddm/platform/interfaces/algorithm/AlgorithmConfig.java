a247180ba82f2ab6b4148400a4c157c9afa1f538
Mikolaj Markiewicz <mikolaj.markiewicz@decerto.com>
2021-11-28 17:47:38 +0100

    Self describing algorithms in terms of parameters
    - for now optional, but in future will be required (remove default)
    - added few information about algorithms to be filled in the impl.

--- platform/model-interface/src/main/java/pl/edu/pw/ddm/platform/interfaces/algorithm/AlgorithmConfig.java
+++ platform/model-interface/src/main/java/pl/edu/pw/ddm/platform/interfaces/algorithm/AlgorithmConfig.java
@@ -1,7 +1,83 @@
 package pl.edu.pw.ddm.platform.interfaces.algorithm;
 
+import lombok.Value;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public interface AlgorithmConfig {
 
     DdmPipeline pipeline();
+
+    /**
+     * If the algorithm works only with numerical data
+     */
+    default boolean onlyNumerical() {
+        return false;
+    }
+
+    /**
+     * If the algorithm works only with nominal data
+     */
+    default boolean onlyNominal() {
+        return false;
+    }
+
+    /**
+     * If the algorithm works only on single node - non-distributed implementation
+     */
+    default boolean onlySingleNode() {
+        return false;
+    }
+
+    /**
+     * List of available parameters used by the algorithm with description
+     */
+    default List<CustomParamDesc> availableParameters() {
+        return Collections.singletonList(CustomParamDesc.of("dummy", Void.class, null, "no desc provided"));
+    }
+
+    default String printAvailableParameters() {
+        List<CustomParamDesc> params = availableParameters();
+        if (params == null) {
+            return "no params described";
+        } else {
+            StringBuilder str = new StringBuilder();
+            for (CustomParamDesc param : params) {
+                str.append(param.key);
+                str.append(" [");
+                str.append(param.type.getSimpleName());
+                str.append(", default=");
+                str.append(param.defaultValue);
+                str.append("]: ");
+                str.append(param.desc);
+                Arrays.stream(param.options)
+                        .forEach(o -> str.append(System.lineSeparator())
+                                .append("  - ")
+                                .append(o));
+                str.append(System.lineSeparator());
+            }
+            return str.toString();
+        }
+    }
+
+    @Value
+    class CustomParamDesc {
+
+        public static CustomParamDesc of(String key,
+                                         Class<?> type,
+                                         String defaultValue,
+                                         String desc,
+                                         String... options) {
+            return new CustomParamDesc(key, type, defaultValue, desc, options);
+        }
+
+        String key;
+        Class<?> type;
+        String defaultValue;
+        String desc;
+        String[] options;
+    }
 
 }
